fd643c26b90e8568d50ceefc0b16be7816a730ff
maryamalrubaye <maryamalrubaye18@gmail.com>
2021-08-28 00:46:20 +0300

    adding style

---
+++ src/Components/UI/Input.js
@@ -0,0 +1,8 @@
+import classes from "./Input.module.css"
+const Input = props => {
+  return <div className={classes.input}>
+<label htmlFor={classes.input.id}>{props.label}</label>
+<input {...props.input}/>
+  </div>
+};
+export default Input;
